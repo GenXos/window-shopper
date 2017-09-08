f38caafa910de404daaf0913f830da5fbc59b3b7
Todd Fields <toddwfields@gmail.com>
2017-09-08 14:39:16 -0500

    created custom text field

---
+++ window-shopper/controller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Todd Fields on 2017-09-08.
+//  Copyright © 2017 Todd Fields. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
