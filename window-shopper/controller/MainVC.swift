9b06a61bc4d32eb71f7ee203d4fa9f8a4f0d0986
Todd Fields <toddwfields@gmail.com>
2017-09-10 11:12:19 -0500

    Made custom text field IBDesignable. Created input accessory button

--- window-shopper/controller/MainVC.swift
+++ window-shopper/controller/MainVC.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    
+    // MARK: - Initialization
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calculateButton.backgroundColor = #colorLiteral(red: 1, green: 0.5383794479, blue: 0.1740443169, alpha: 1)
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTextField.inputAccessoryView = calculateButton
+        priceTextField.inputAccessoryView = calculateButton
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Functions
+    @objc func calculate() {
+        
+        print("we got here!")
     }
 
 
